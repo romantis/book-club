a9c9d4c8affde4f395ad4ee27dcb595713cee213
Roman Tischenko <udatos@gmail.com>
2016-07-11 20:16:14 +0300

    added type annotation

--- src/Navbar.elm
+++ src/Navbar.elm
@@ -3,7 +3,7 @@ module Navbar exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (class, href)
 
-
+global :  Html msg
 global  =
   nav [ class "" ]
     [ a [ class "", href "#" ] [ text "Home" ]
