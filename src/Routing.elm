c20a277e631a12ab7bffa777191130e2c6642791
Roman Tischenko <udatos@gmail.com>
2016-07-12 13:06:33 +0300

    swiched to hop routing

--- src/Routing.elm
+++ src/Routing.elm
@@ -1,9 +1,15 @@
 module Routing exposing (..)
 
-import String
+--import String
 import Navigation
 import UrlParser exposing (..)
 
+import Hop exposing (makeUrl, makeUrlFromLocation, matchUrl, setQuery)
+import Hop.Types exposing (Config, Query, Location, PathMatcher, Router)
+import Hop.Matchers exposing (..)
+
+
+import Debug
 
 type Route
     = HomeRoute
@@ -11,31 +17,68 @@ type Route
     | NotFoundRoute
 
 
+type Msg
+    = NavigateTo String
+    | SetQuery Query
+
+
+type alias Model =
+    { location : Location
+    , route : Route
+    }
+
 matchers : Parser (Route -> a) a
 matchers =
-    oneOf
-        [ format HomeRoute (s "")
-        , format ComponentRoute (s "component")
-        ]
+    [ match1 HomeRoute ""
+    , match1 ComponentRoute ""
+    ]
+
+routerConfig : Config Route
+routerConfig =
+    { hash = True
+    , basePath = ""
+    , matchers = matchers
+    , notFound = NotFoundRoute
+    }
 
 
-hashParser : Navigation.Location -> Result String Route
-hashParser location =
-    location.hash
-        |> String.dropLeft 1
-        |> parse identity matchers
 
 
-parser : Navigation.Parser (Result String Route)
-parser =
-    Navigation.makeParser hashParser
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case (Debug.log "msg" msg) of
+        NavigateTo path ->
+            let
+                command =
+                    -- First generate the URL using your router config
+                    -- Then generate a command using Navigation.newUrl
+                    makeUrl routerConfig path
+                        |> Navigation.newUrl
+            in
+                ( model, command )
+
+        SetQuery query ->
+            let
+                command =
+                    -- First modify the current stored location record (setting the query)
+                    -- Then generate a URL using makeUrlFromLocation
+                    -- Finally, create a command using Navigation.newUrl
+                    model.location
+                        |> setQuery query
+                        |> makeUrlFromLocation routerConfig
+                        |> Navigation.newUrl
+            in
+                ( model, command )
 
 
-routeFromResult : Result String Route -> Route
-routeFromResult result =
-    case result of
-        Ok route ->
-            route
 
-        Err string ->
-            NotFoundRoute
+urlParser : Navigation.Parser ( Route, Hop.Types.Location )
+urlParser =
+    Navigation.makeParser (.href >> matchUrl routerConfig)
+
+
+urlUpdate : ( Route, Hop.Types.Location ) -> Model -> ( Model, Cmd Msg )
+urlUpdate ( route, location ) model =
+    ( { model | route = route, location = location }, Cmd.none )
+
+
