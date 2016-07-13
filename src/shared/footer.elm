133fd06818e537346bd362307da15572ac9492e3
Roman Tischenko <udatos@gmail.com>
2016-07-13 21:09:23 +0300

    added css classes to footer, and changed a bit it's content

--- src/shared/footer.elm
+++ src/shared/footer.elm
@@ -5,8 +5,11 @@ import Html.Attributes exposing (class, href, id)
 
 view : Html msg
 view =
-  footer [id "footer", class "bc-footer"]
-    [ text "All code for this site is open source and written in BookClub. "
+  footer 
+    [ id "footer"
+    , class "uk-block uk-block-secondary uk-contrast uk-text-center"
+    ]
+    [ text "All code for this site is open source and written in Elm. "
     , a [ class "", href "#" ] [ text "Check it out!" ]
     , text " — © 2016 Roman Tischenko"
     ]
