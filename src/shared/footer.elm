2eeae39fba904b0162202a25b47b99dc899c912d
Roman Tischenko <udatos@gmail.com>
2016-07-20 17:38:58 +0300

    added url to github in footer

--- src/shared/footer.elm
+++ src/shared/footer.elm
@@ -1,7 +1,7 @@
 module Shared.Footer exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, href, id)
+import Html.Attributes exposing (class, href, id, target)
 
 view : Html msg
 view =
@@ -10,7 +10,7 @@ view =
     , class "uk-block uk-block-secondary uk-contrast uk-text-center"
     ]
     [ text "All code for this site is open source and written in Elm. "
-    , a [ class "", href "#" ] [ text "Check it out!" ]
+    , a [ class "", href "https://github.com/romantis/elm-starter", target "_blank" ] [ text "Check it out!" ]
     , text " — © 2016 Roman Tischenko"
     ]
 
