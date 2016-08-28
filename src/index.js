5e99d02247e5f6457fe28b5bdf4a148d1a697fbd
Roman Tischenko <udatos@gmail.com>
2016-08-28 19:26:56 +0300

    add google-maps to layout

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 require('uikit/dist/css/uikit.almost-flat.min.css');
 require('./main.css');
 
+const gm = require('google-maps');
 
 
 // Require index.html so it gets copied to dist
@@ -14,3 +15,15 @@ var mountNode = document.getElementById('main');
 
 // The third value on embed are the initial values for incomming ports into Elm
 var app = Elm.Main.embed(mountNode);
+
+app.ports.loadmap.subscribe(([latitude, longitude]) => {
+    gm.load((google) => {
+        console.log(latitude, longitude)
+        let  options = {
+            center: new google.maps.LatLng(latitude, longitude),
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        new google.maps.Map(document.getElementById('place-map'), options);
+    });
+})
--- src/meetup/main.elm
+++ src/meetup/main.elm
@@ -1,7 +1,7 @@
 module Meetup.Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (src, class, style)
+import Html.Attributes exposing (src, class, style, id)
 import Html.App as App
 
 import Http
@@ -9,8 +9,11 @@ import Task
 import Json.Decode as Decode exposing ((:=), andThen)
 import Date exposing (Date, Month(..))
 import Time exposing (Time)
+
 import Errors.Main as Errors
 import Date.Format as Date
+
+import Ports exposing (loadmap)
 
 
 (=>) : a -> b -> ( a, b )
@@ -51,11 +54,11 @@ init =
     Model Nothing Errors.init
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
+update msg ({meetup, errors} as model) =
     case msg of
         FetchAllDone meetup ->
             ( {model | meetup = Just meetup}
-            , Cmd.none
+            , loadmap (meetup.place.latitude, meetup.place.longitude)
             )
 
 
@@ -93,7 +96,7 @@ meetupView : Meetup -> Errors.Model -> Html Msg
 meetupView meetup errors=
     section 
         [ class "bc-min-height"]
-        [ meetupHeaderView
+        [ meetupHeaderLayout
             [ h1 [ class "uk-heading-large"] 
                 [ text meetup.title
                 , span [class "uk-text-primary uk-margin-left"] [text "Meetup"] 
@@ -104,21 +107,42 @@ meetupView meetup errors=
                 ]
             ]
             
-        , div [class "uk-width-small-1-2 uk-container-center uk-block"]
-            [ h2 [] [ text "Meetup place"] 
-            , p [] [text meetup.place.city]
-            , h2 [class ""] [ text "Description"]
-            , p [ class "" ]
-                [ text meetup.description
+        , div [ class "uk-grid"] 
+            [ descriptionLayout
+                [ h2 [] [ text "Meetup place"] 
+                , p [] [text ( meetup.place.city ++ ", "++ meetup.place.country  ) ]
+                , h2 [class ""] [ text "Description"]
+                , p [ class "" ]
+                    [ text meetup.description
+                    ] 
                 ] 
-            ] 
-            
+            , div 
+                [ id "place-map"
+                , class "uk-width-small-1-1 uk-width-medium-1-2 uk-width-large-2-3"
+                , style ["background" => "#eee"] 
+                ]
+                []
+            ]
+
         , App.map ErrMsg (Errors.view errors)
         ] 
 
 
+descriptionLayout : List (Html Msg) -> Html Msg
+descriptionLayout content = 
+    div [class "uk-width-small-1-1 uk-width-medium-1-2 uk-width-large-1-3"]
+        [ div 
+            [ class "uk-block"
+            , style [ "padding-left"=> "1em"
+                    , "padding-right"=> "1em"
+                    ]
+            ] 
+            content 
+        ]
 
-meetupHeaderView content =
+
+meetupHeaderLayout : List (Html Msg) -> Html Msg
+meetupHeaderLayout content =
     header 
         [ style [ "height" => "30vh"]
         , class "bg-carbon-fibre uk-contrast uk-flex uk-flex-center uk-flex-middle uk-flex-column"
@@ -163,3 +187,4 @@ memberDecoder =
 sub : Model -> Sub Msg 
 sub model =
     Sub.map ErrMsg (Errors.sub model.errors)
+
