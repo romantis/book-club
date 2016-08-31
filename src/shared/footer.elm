738741c48ddf78223ba4bafd7f8b698a0c89030b
Roman Tischenko <udatos@gmail.com>
2016-08-31 10:59:57 +0300

    update link to github repo

--- src/shared/footer.elm
+++ src/shared/footer.elm
@@ -10,7 +10,7 @@ view =
     , class "uk-block uk-block-secondary uk-contrast uk-text-center"
     ]
     [ text "All code for this site is open source and written in Elm. "
-    , a [ class "", href "https://github.com/romantis/elm-starter", target "_blank" ] [ text "Check it out!" ]
+    , a [ class "", href "https://github.com/romantis/book-club", target "_blank" ] [ text "Check it out!" ]
     , text " — © 2016 Roman Tischenko"
     ]
 
