14456a6695f6212de402befc590473e61a8080ec
Roman Tischenko <udatos@gmail.com>
2016-11-25 14:34:32 +0200

    Setup 'open layers' map

--- src/index.js
+++ src/index.js
@@ -15,6 +15,20 @@ var mountNode = document.getElementById('main');
 // The third value on embed are the initial values for incomming ports into Elm
 var app = Elm.Main.embed(mountNode);
 
-app.ports.loadmap.subscribe(([latitude, longitude]) => {
-    console.log("latitude: %s \n longitude: %s", latitude, longitude)    
+app.ports.loadmap.subscribe(([longitude, latitude]) => {
+    setTimeout(() => {
+      var map = new ol.Map({
+          target: 'place-map',
+          layers: [
+            new ol.layer.Tile({
+              source: new ol.source.OSM()
+            })
+          ],
+          view: new ol.View({
+            center: ol.proj.fromLonLat([longitude, latitude]),
+            zoom: 13
+          })
+        });
+    }
+    , 50);
 })
